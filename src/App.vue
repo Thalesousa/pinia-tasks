14658bd4e10fde780219df5da21e5c6bb05b8ccd
Thales Sousa <thales.bezerra.ter@estacio.br>
2024-05-21 17:15:55 -0300

    feat: init project pinia

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <!-- heading -->
+
+    <header>
+      <img src="/public/pinia-logo.svg" alt="pinia logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
