f30ed2fb7aea100117ee48c1a810735290209132
Thales Sousa <thales.bezerra.ter@estacio.br>
2024-05-21 17:25:41 -0300

    feat: install pinia & create store

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <main>
     <!-- heading -->
-
     <header>
-      <img src="/public/pinia-logo.svg" alt="pinia logo" />
+      <img src="/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia Tasks</h1>
     </header>
   </main>
---
+++ src/stores/TaskStore.js
@@ -0,0 +1,11 @@
+const { defineStore } = require('pinia');
+
+export const useTaskStore = defineStore('taskStore', {
+  state: () => ({
+    tasks: [
+      { id: 1, title: 'buy some milk', isFav: false },
+      { id: 2, title: 'play Gloomhaven', isFav: true },
+    ],
+    name: 'Yoshi',
+  }),
+});
